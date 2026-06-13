4cf45d6b8eb92e5ded4819e997bbfb42979c9016
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-06-12 21:48:42 -0300

    Agregar enumeración TipoMovimiento con valores ENTRADA y SALIDA para manejo de stock

--- src/main/java/ar/edu/unlar/tp5_inventario/model/TipoMovimiento.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/model/TipoMovimiento.java
@@ -1,5 +1,9 @@
 package ar.edu.unlar.tp5_inventario.model;
 
 public enum TipoMovimiento {
-
+    /** Incrementa el stock disponible. */
+    ENTRADA,
+    
+    /** Decrementa el stock disponible. */
+    SALIDA
 }
