542d0d1b2620b32e528f73f490484611f948461c
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-06-12 21:54:55 -0300

    Agregar valores a la enumeración NivelAlerta para el manejo de stock

--- src/main/java/ar/edu/unlar/tp5_inventario/model/NivelAlerta.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/model/NivelAlerta.java
@@ -1,5 +1,12 @@
 package ar.edu.unlar.tp5_inventario.model;
 
 public enum NivelAlerta {
-
+    /** El stock es mayor o igual al minimo configurado. */
+    NORMAL,    
+    
+    /** El stock está por debajo del minimo, pero es mayor o igual al critico. */
+    BAJO,      
+    
+    /** El stock es inferior al umbral critico configurado. */
+    CRITICO    
 }
