ec6c571a23d6fea891b7412e821b7e7432783022
Ismael Flores <117777526+IsmaDeveloper16@users.noreply.github.com>
2026-06-13 23:01:02 -0300

    Convertir CategoriaRequest a un registro y agregar validaciones para los atributos nombre y descripcion

--- src/main/java/ar/edu/unlar/tp5_inventario/dto/CategoriaRequest.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/dto/CategoriaRequest.java
@@ -1,5 +1,13 @@
 package ar.edu.unlar.tp5_inventario.dto;
 
-public class CategoriaRequest {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-}
+public record CategoriaRequest(
+        @NotBlank(message = "El nombre de la categoría es obligatorio")
+        @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
+        String nombre,
+
+        @Size(max = 200, message = "La descripción no puede exceder los 200 caracteres")
+        String descripcion
+) {}
