19b14ada364eea5d6ecec35874e24ab58f7154a7
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-06-13 23:47:15 -0300

    Implementar InMemoryCategoriaRepository como repositorio en memoria para la entidad Categoria

--- src/main/java/ar/edu/unlar/tp5_inventario/repository/InMemoryCategoriaRepository.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/repository/InMemoryCategoriaRepository.java
@@ -1,5 +1,10 @@
 package ar.edu.unlar.tp5_inventario.repository;
 
-public class InMemoryCategoriaRepository {
+import ar.edu.unlar.tp5_inventario.model.Categoria;
+import org.springframework.stereotype.Repository;
 
+//Implementación en memoria del repositorio de Categorías.
+
+@Repository
+public class InMemoryCategoriaRepository extends GenericInMemoryRepository<Categoria, Long> implements CategoriaRepository {
 }
