c0a7e7c1a98ac07089126e6743568e3cd09791bd
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-06-13 23:38:05 -0300

    Definir la interfaz IGenericRepository para operaciones CRUD básicas

--- src/main/java/ar/edu/unlar/tp5_inventario/repository/IGenericRepository.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/repository/IGenericRepository.java
@@ -1,5 +1,20 @@
 package ar.edu.unlar.tp5_inventario.repository;
 
-public class IGenericRepository {
+import java.util.List;
+import java.util.Optional;
 
+// Interfaz genérica para repositorios que define las operaciones CRUD basicas
+public interface IGenericRepository<T, ID> {
+
+    List<T> findAll();
+
+    Optional<T> findById(ID id);
+
+    T save(T entity);
+
+    void deleteById(ID id);
+
+    boolean existsById(ID id);
+
+    long count();
 }
