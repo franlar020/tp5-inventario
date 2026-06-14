bca872334715c38af2077a4291dc04639c94f3b8
Ismael Flores <117777526+IsmaDeveloper16@users.noreply.github.com>
2026-06-14 00:02:35 -0300

    Agregar la clase ResourceNotFoundException para manejar excepciones de recursos no encontrados

--- src/main/java/ar/edu/unlar/tp5_inventario/exception/ResourceNotFoundException.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/exception/ResourceNotFoundException.java
@@ -1,5 +1,12 @@
 package ar.edu.unlar.tp5_inventario.exception;
 
-public class ResourceNotFoundException {
+// Excepción lanzada cuando se intenta acceder o manipular un recurso que no existe.
 
+public class ResourceNotFoundException extends RuntimeException {
+
+    // Construye una nueva ResourceNotFoundException con el mensaje especificado.
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
