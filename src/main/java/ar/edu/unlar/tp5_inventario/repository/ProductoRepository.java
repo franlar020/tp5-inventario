db9e1571266dfb04a7a1ad9c39a055ebb7cf249a
Ismael Flores <117777526+IsmaDeveloper16@users.noreply.github.com>
2026-06-13 23:49:42 -0300

    Definir la interfaz ProductoRepository como contrato específico para el repositorio de Productos

--- src/main/java/ar/edu/unlar/tp5_inventario/repository/ProductoRepository.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/repository/ProductoRepository.java
@@ -1,5 +1,12 @@
 package ar.edu.unlar.tp5_inventario.repository;
 
-public class ProductoRepository {
 
+import ar.edu.unlar.tp5_inventario.model.Producto;
+import java.util.List;
+
+// Contrato específico para el repositorio de Productos.
+
+public interface ProductoRepository extends IGenericRepository<Producto, Long> {
+    List<Producto> findByCategoria(Long categoriaId); // Filtra los productos que pertenecen a una categoría específica.
+    List<Producto> buscarPorNombre(String texto); // Realiza una búsqueda textual (case-insensitive) sobre el nombre de los productos.
 }
