3b93a9937540de0fb9f2c18a7346c625a29d14b6
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-06-13 23:11:41 -0300

    Convertir ProductoResponse a un registro para mejorar la exposición de datos

--- src/main/java/ar/edu/unlar/tp5_inventario/dto/ProductoResponse.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/dto/ProductoResponse.java
@@ -1,5 +1,13 @@
 package ar.edu.unlar.tp5_inventario.dto;
 
-public class ProductoResponse {
+// DTO para la exposición de datos de un Producto hacia el cliente HTTP.
 
-}
+
+public record ProductoResponse(
+        Long id,
+        String nombre,
+        String descripcion,
+        double precio,
+        int stock,
+        CategoriaResponse categoria
+) {}
