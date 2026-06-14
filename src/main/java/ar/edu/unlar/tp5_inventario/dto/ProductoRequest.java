06ab528749c99a6513c4db09a140b07caffdf464
Ismael Flores <117777526+IsmaDeveloper16@users.noreply.github.com>
2026-06-13 23:08:20 -0300

    Convertir ProductoRequest a un registro y agregar validaciones para los atributos nombre, descripcion, precio, stockInicial y categoriaId

--- src/main/java/ar/edu/unlar/tp5_inventario/dto/ProductoRequest.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/dto/ProductoRequest.java
@@ -1,5 +1,25 @@
 package ar.edu.unlar.tp5_inventario.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 
-public class ProductoRequest {
+// DTO para la recepción de datos al crear o actualizar un Producto.
 
-}
+public record ProductoRequest(
+        @NotBlank(message = "El nombre es obligatorio")
+        @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
+        String nombre,
+
+        @Size(max = 500, message = "La descripción no puede exceder los 500 caracteres")
+        String descripcion,
+
+        @PositiveOrZero(message = "El precio debe ser >= 0")
+        double precio,
+
+        @PositiveOrZero(message = "El stock inicial debe ser >= 0")
+        int stockInicial,
+
+        @NotNull(message = "La categoría es obligatoria")
+        Long categoriaId
+) {}
