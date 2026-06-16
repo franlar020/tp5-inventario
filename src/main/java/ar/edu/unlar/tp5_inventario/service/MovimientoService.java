594a6c43bf68222f16efdf4ec448293e3a8cb5a4
VirginiaVeraH <virginiavh454@gmail.com>
2026-06-15 22:04:58 -0300

    Movimiento service

--- src/main/java/ar/edu/unlar/tp5_inventario/service/MovimientoService.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/service/MovimientoService.java
@@ -1,5 +1,14 @@
 package ar.edu.unlar.tp5_inventario.service;
 
+import ar.edu.unlar.tp5_inventario.dto.MovimientoRequest;
+import ar.edu.unlar.tp5_inventario.dto.MovimientoResponse;
+import java.util.List;
+
+// Contrato para el servicio de gestión de movimientos (entradas/salidas).
+
 public interface MovimientoService {
 
+    MovimientoResponse registrarMovimiento(MovimientoRequest request);
+
+    List<MovimientoResponse> obtenerHistorialPorProducto(Long productoId);
 }
