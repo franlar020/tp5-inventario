81584e092b2602b8f29c15f7882c50b3df162101
Ismael Flores <117777526+IsmaDeveloper16@users.noreply.github.com>
2026-06-15 22:18:10 -0300

    Refactor StockConfig to use record and add ConfigurationProperties annotation

--- src/main/java/ar/edu/unlar/tp5_inventario/config/StockConfig.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/config/StockConfig.java
@@ -1,5 +1,7 @@
 package ar.edu.unlar.tp5_inventario.config;
 
-public class StockConfig {
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@ConfigurationProperties(prefix = "inventario.stock")
+public record StockConfig(int minimo, int critico) {
 }
