7d55ac3f29322497d9d8429f9c8cd83320565838
VirginiaVeraH <virginiavh454@gmail.com>
2026-06-15 22:43:34 -0300

    Alerta service

--- src/main/java/ar/edu/unlar/tp5_inventario/service/AlertaService.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/service/AlertaService.java
@@ -1,5 +1,11 @@
 package ar.edu.unlar.tp5_inventario.service;
 
+import ar.edu.unlar.tp5_inventario.dto.AlertaStockResponse;
+import java.util.List;
+
+// Contrato para el servicio de monitoreo de alertas de stock.
+ 
 public interface AlertaService {
 
+    List<AlertaStockResponse> obtenerProductosConStockBajo();
 }
