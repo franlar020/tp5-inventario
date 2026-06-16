9dd76ae3271dce05969e5792bdb889dc72cd9b76
VirginiaVeraH <virginiavh454@gmail.com>
2026-06-15 21:42:59 -0300

    Creación categoría service

--- src/main/java/ar/edu/unlar/tp5_inventario/service/CategoriaService.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/service/CategoriaService.java
@@ -1,5 +1,14 @@
 package ar.edu.unlar.tp5_inventario.service;
 
+import ar.edu.unlar.tp5_inventario.dto.CategoriaRequest;
+import ar.edu.unlar.tp5_inventario.dto.CategoriaResponse;
+import java.util.List;
+
 public interface CategoriaService {
 
+    List<CategoriaResponse> obtenerTodas();
+    CategoriaResponse obtenerPorId(Long id);
+    CategoriaResponse crear(CategoriaRequest request);
+    CategoriaResponse actualizar(Long id, CategoriaRequest request);
+    void eliminar(Long id);
 }
