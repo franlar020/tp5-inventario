8209aad3970126b0a5fefd9840be94c6d0fdea10
VirginiaVeraH <virginiavh454@gmail.com>
2026-06-19 21:52:04 -0300

    Admin controller

--- src/main/java/ar/edu/unlar/tp5_inventario/controller/AdminController.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/controller/AdminController.java
@@ -1,5 +1,26 @@
 package ar.edu.unlar.tp5_inventario.controller;
 
+import ar.edu.unlar.tp5_inventario.dto.MetricaPerformance;
+import ar.edu.unlar.tp5_inventario.service.PerformanceReportService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
 public class AdminController {
 
+    private final PerformanceReportService performanceReportService;
+
+    public AdminController(PerformanceReportService performanceReportService) {
+        this.performanceReportService = performanceReportService;
+    }
+
+    @GetMapping("/performance-report")
+    public ResponseEntity<List<MetricaPerformance>> generarReporte() {
+        return ResponseEntity.ok(performanceReportService.generarReporte());
+    }
 }
