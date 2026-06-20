c22a4defd2258cbc41b9c7b324574c71efd094d8
Francisco Antonio Gonzalez <franlar020@gmail.com>
2026-06-19 21:47:07 -0300

    Implementar el controlador de alertas para gestionar notificaciones de stock bajo

--- src/main/java/ar/edu/unlar/tp5_inventario/controller/AlertaController.java
+++ src/main/java/ar/edu/unlar/tp5_inventario/controller/AlertaController.java
@@ -1,5 +1,26 @@
 package ar.edu.unlar.tp5_inventario.controller;
 
+import ar.edu.unlar.tp5_inventario.dto.AlertaStockResponse;
+import ar.edu.unlar.tp5_inventario.service.AlertaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/alertas")
 public class AlertaController {
 
+    private final AlertaService alertaService;
+
+    public AlertaController(AlertaService alertaService) {
+        this.alertaService = alertaService;
+    }
+
+    @GetMapping("/stock-bajo")
+    public ResponseEntity<List<AlertaStockResponse>> obtenerAlertas() {
+        return ResponseEntity.ok(alertaService.obtenerProductosConStockBajo());
+    }
 }
